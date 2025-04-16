697e6204ebeccccb1b22000fb043f2d05b9bad65
MuriloNovaes <murilob.novaes1@gmail.com>
2025-04-15 21:51:54 -0300

    "Adicionando o data_processing em funcionamento, e separando a main por etapas."

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from src.fetch_data import obter_precos
 from src.utils import ver_resumo
+from src.data_processing import preparar_dados
 
+# Passo 1: Baixar os dados
 dados = obter_precos(['PETR4.SA', 'ITUB4.SA'])
-ver_resumo(dados)
+
+# Passo 2: Tratar os dados
+retornos = preparar_dados(dados)
+
+# Passo 3: Visualizar os dados tratados
+ver_resumo(retornos)
 
