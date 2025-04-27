0bd8e9dac6930411337661e063856b878943b1ec
MuriloNovaes <murilob.novaes1@gmail.com>
2025-04-27 16:23:46 -0300

    feat: adiciona análise de risco-retorno

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.fetch_data import obter_precos
 from src.utils import ver_resumo
 from src.data_processing import preparar_dados
+from src.utils import analise_risco_retorno
+import pandas as pd
 
 # Passo 1: Baixar os dados
 dados = obter_precos(['PETR4.SA', 'ITUB4.SA'])
@@ -10,4 +12,6 @@ retornos = preparar_dados(dados)
 
 # Passo 3: Visualizar os dados tratados
 ver_resumo(retornos)
-
+# Após ver_resumo(retornos), adicione:
+print("\n📊 Métricas  de Risco-Retorno: ")
+print(analise_risco_retorno(retornos))
--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,19 @@ def ver_resumo(df):
     print("📊 Resumo do DataFrame (5 primeiras e 5 últimas):")
     print(pd.concat([df.head(), df.tail()]))
 
+
+def analise_risco_retorno(retornos):
+    """
+    Mostra o retorno medio e volatilidade de cada ação;
+
+    Parâmetros:
+    - retornos (pandas.DataFrame): DataFrame com os retornos diários das ações.
+
+    Retorna:
+    - DataFrame com métricas de cada ação. 
+    """
+    analise = pd.DataFrame({
+        'Retorno Médio (%)': retornos.mean() * 100,
+        'Volatilidade (%)': retornos.std() * 100 # Riscp (desvio padrão) 
+    })
+    return analise.sort_values(by='Retorno Médio (%)', ascending=False)
